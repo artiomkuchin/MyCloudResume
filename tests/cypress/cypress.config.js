dca31b3889ae38f8961552d00244fd4f3c54577d
Artiom Kuchin <akuchinaws@gmail.com>
2023-04-29 16:17:08 -0500

    update cypress.json to cypress.config.js

---
+++ tests/cypress/cypress.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  baseUrl: "http://localhost:3000",
+  integrationFolder: "tests/cypress/integration",
+  testFiles: "**/*.{js,jsx,ts,tsx}",
+  screenshotsFolder: "tests/cypress/screenshots",
+  videosFolder: "tests/cypress/videos",
+  supportFile: "tests/cypress/support/index.{js,ts}",
+  pluginsFile: "tests/cypress/plugins/index.{js,ts}"
+};
